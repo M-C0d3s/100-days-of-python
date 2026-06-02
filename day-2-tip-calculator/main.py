5e8096f1d341c82d606b2252701545a0c3c3d28b
M-C0d3s <258472849+M-C0d3s@users.noreply.github.com>
2026-06-02 12:33:58 +0330

    Create main.py
    Add Day 2 tip calculator project

---
+++ day-2-tip-calculator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!\n")
+total_bill = float(input("What was the total bill? $"))
+tip_percent = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
+people_num = int(input("How many people to split the bill? "))
+calculation = (total_bill) / (people_num) * (1 + (tip_percent) / 100)
+final_answer = "{:.2f}".format(calculation)
+print(f"Each person should pay: ${final_answer}")
